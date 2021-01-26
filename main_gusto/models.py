7605a420a9ac8b755ddbc28dcd46b419d624ffab
Dima Didkivskyi <keenjee90@gmail.com>
2021-01-26 17:43:34 +0200

    Create db migration and admin control panel

--- main_gusto/models.py
+++ main_gusto/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    category_order = models.IntegerField()
+    is_visible = models.BooleanField(default=True)
+    is_special = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.title} : {self.category_order}'
+
+class Dishes(models.Model):
+    
--- main_gusto/views.py
+++ main_gusto/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Category
 
 
 def main(request):
-    return render(request, 'index.html')
+    categories = Category.objects.all()
+    return render(request, 'index.html', context={'categories': categories})
