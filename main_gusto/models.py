bb777ebc5d2980070fee47256a9bfc0e991818fd
Dima Didkivskyi <keenjee90@gmail.com>
2021-01-28 16:45:38 +0200

    Add DIshes table and edit index.html

--- main_gusto/models.py
+++ main_gusto/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from uuid import uuid4
+import os
 
 
 class Category(models.Model):
@@ -10,5 +12,17 @@ class Category(models.Model):
     def __str__(self):
         return f'{self.title} : {self.category_order}'
 
+
 class Dishes(models.Model):
-    
+
+    def get_file_name_dishes(self, filename):
+        ext = filename.split('.')[-1]
+        filename = f'{uuid4()}.{ext}'
+        return os.path.join('images/dishes/', filename)
+
+    title = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    is_visible = models.BooleanField(default=True)
+    description = models.CharField(max_length=300, null=True)
+    photo = models.ImageField(upload_to=get_file_name_dishes)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
--- main_gusto/views.py
+++ main_gusto/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from .models import Category
+from .models import Category, Dishes
 
 
 def main(request):
-    categories = Category.objects.all()
+    categories = Category.objects.filter(
+        is_visible=True).order_by('category_order')
+    for item in categories:
+        item.dishes = Dishes.objects.filter(category=item.pk)
     return render(request, 'index.html', context={'categories': categories})
