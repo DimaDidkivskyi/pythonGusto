3f7333ec5303d77bb435e6b4fa12037c79d926d6
DimaDidkovskiy <44492950+DimaDidkovskiy@users.noreply.github.com>
2021-04-11 16:39:11 +0300

    Update settings.py

--- Gusto/settings.py
+++ Gusto/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'ud^t#tahgc63%l0!+_$hwe7$p8d-kvx%=iigm-@!wh9+qld%9n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gusto-didkivskyi.herokuapp.com']
 
 
 # Application definition
