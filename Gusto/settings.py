a9e369ef5af4fa996e668229d5e16720f93d3bc6
Dima Didkivskyi <keenjee90@gmail.com>
2021-04-13 18:06:45 +0300

    Change media and static roots

--- Gusto/settings.py
+++ Gusto/settings.py
@@ -151,12 +151,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
-
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = (os.path.join(BASE_DIR, 'assets'))
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'))
