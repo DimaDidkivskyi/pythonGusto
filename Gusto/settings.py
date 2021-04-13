64590cc8305475c9625b09731eb25e1e01a63207
Dima Didkivskyi <keenjee90@gmail.com>
2021-04-13 17:55:54 +0300

    Add staticfiles storage

--- Gusto/settings.py
+++ Gusto/settings.py
@@ -151,10 +151,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'))
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
